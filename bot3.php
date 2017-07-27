b9849533a6246f90a3ffc206deba1235090e4b43
shinebue <30455764+shinebue@users.noreply.github.com>
2017-07-27 18:46:56 +0700

    f

--- bot3.php
+++ bot3.php
@@ -6,9 +6,38 @@ include ('line-bot.php');
 
 $channelSecret = 'f475cac7a9e191fad18a48480516b2a2';
 $access_token  = 'ltjBGYjoeVWUGZrtlP2nOS6m28fwAMY0JXAlHwkIPer2M+C2MmmGtMbaQhyZE2Dr29IFqQIzU+46OZLM+SnNOXywq27vpuGAMPiUt1GqD/Mo7qmqPtnxjwlAGpwntObMoXZ7rYPY0JbZeQALuH5ILgdB04t89/1O/w1cDnyilFU=';
-
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
 
 $bot = new BOT_API($channelSecret, $access_token);
+$_msg = $arrJson['events'][0]['message']['text'];
+
+
+if (strpos($_msg, 'ID') !== false) {
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->source->userId));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	exit();
+
+} else {
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	exit();
+	
+}
 	
 if (!empty($bot->isEvents)) {
 		
