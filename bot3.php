81d19fcac749530ac4b83886e844b6a5a73eb4b9
shinebue <30455764+shinebue@users.noreply.github.com>
2017-07-27 18:01:32 +0700

    m

--- bot3.php
+++ bot3.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include (line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = 'f475cac7a9e191fad18a48480516b2a2';
 $access_token  = 'ltjBGYjoeVWUGZrtlP2nOS6m28fwAMY0JXAlHwkIPer2M+C2MmmGtMbaQhyZE2Dr29IFqQIzU+46OZLM+SnNOXywq27vpuGAMPiUt1GqD/Mo7qmqPtnxjwlAGpwntObMoXZ7rYPY0JbZeQALuH5ILgdB04t89/1O/w1cDnyilFU=';
