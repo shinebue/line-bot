b7ac44001d49ac6eb878ebd1390f409d681bec3a
shinebue <30455764+shinebue@users.noreply.github.com>
2017-07-27 18:14:27 +0700

    mm

--- bot3.php
+++ bot3.php
@@ -8,12 +8,19 @@ $channelSecret = 'f475cac7a9e191fad18a48480516b2a2';
 $access_token  = 'ltjBGYjoeVWUGZrtlP2nOS6m28fwAMY0JXAlHwkIPer2M+C2MmmGtMbaQhyZE2Dr29IFqQIzU+46OZLM+SnNOXywq27vpuGAMPiUt1GqD/Mo7qmqPtnxjwlAGpwntObMoXZ7rYPY0JbZeQALuH5ILgdB04t89/1O/w1cDnyilFU=';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+	$bot->replyMessageNew($bot->replyToken, json_encode('test'));
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบ ตอบกลับ');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	exit();
 
 }
